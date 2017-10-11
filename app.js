34eb7ca04c68426ab26eef6d8537caf448315750
Belchenkov <u608110@gmail.com>
2017-10-11 19:49:42 +0300

    Add Navbar && Bootstap 4

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Index Route
+app.get('/', (req, res) => {
+    const title = 'Welcome to VidJot!';
+    res.render('index', { title });
+});
+
+// About Page
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 const port = 5000;
 
