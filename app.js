055ebc9fa153e07c13d0dff6d71b17df3b2e4792
Aleksey Belchenkov <u608110@gmail.com>
2019-01-19 20:17:25 +0300

    Edit Idea Form

--- app.js
+++ app.js
@@ -56,6 +56,20 @@ app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
 });
 
+// Edit Idea Form
+app.get('/ideas/edit/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+        .then(idea => {
+            res.render('ideas/edit', {
+                idea
+            });
+        })
+        .catch(err => console.log(err));
+});
+
+
 // Process Form
 app.post('/ideas', (req, res) => {
     let errors = [];
